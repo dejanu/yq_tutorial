fd52f20f670da9a072e063a8780d5312d494527f
dejanualex <dejanualexandru@gmail.com>
2022-12-09 00:16:48 +0200

    add wrapper

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # yq_tutorial
-Repo to demo yq usage as a template engine
+Repo to demo yq usage as a template engine:
+- in `yq` expressions are made up of operators and pipes
+- yq mode is `eval`, which allows reading, searching, and editing YAML files
+- `yq`` parses YAML files in the similar way to tree parsing, dividing the YAML files into different node types, which are nested in one another.
 
-
-* yq mode is `eval`, which allows reading, searching, and editing YAML files:
+* Evaluate a YAML file:
 ```bash
 # output the content of personal_data.yaml
 yq personal_data.yaml
@@ -15,11 +17,25 @@ yq '.name' personal_data.yaml
 
 # access nested/indented data
 yq '.street_address[]' personal_data.yml
+
+# get the keys of an value with a map
+yq '.bundleConfig | keys' ex-config.yaml
 ```
 
 * change properties of YAML files:
 ```bash
 # change a field
-yq '.name = "John Doe"' personal_data.yaml
-yq '.street_address.number = 256' personal_data.yaml
+yq '.name = "John Doe"' personal_data.yml
+yq '.street_address.number = 256' personal_data.yml
+```
+### Operators
+
+* to_entries operator:
+
+```bash
+# extract the keys of a YAML file
+yq 'to_entries | .[] | .key' personal_data.yaml
+
+# extract the values of a YAML file
+yq 'to_entries | .[] | .value' personal_data.yaml
 ```
---
+++ yqyml_gen.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# ad-hoc yml linter as alias
+#alias ymllint='python -c "import sys,yaml as y;y.safe_load(open(sys.argv[1]))"'
+
+# display to stdout the script usage
+function usage() {
+    echo "Usage: $0 [options] [file]"
+    echo "Options:"
+    echo "  -h, --help <file>: ask @dejanualex for help"
+    echo "  -c, --content <file> : outputs the content of the yaml file"
+    echo "  -k, --keys <file>: outputs the keys of the yaml file"
+    echo "  -v, --values <file>: outputs the values of the yaml file"
+    echo "  -kv, --keyvalue <file> <key_name>: get the value of the key"
+    echo " -vr, --valuereplace <file> <key_name> <value>: replace the value of the key"
+}
+
+
+# if script only has one argument, display the usage
+if [ $# -eq 1 ]; then
+    usage
+    exit 1
+fi
+
+# get the yaml file as an second argument and check if it is valid
+file=$2
+if [[ $file == *.yaml ]] || [[ $file == *.yml ]]; then
+    # check if the file is valid
+    if [[ $(yamllint $file) ]]; then
+        echo "The argument is a valid yaml file"
+    else
+        echo "The argument is NOT a valid yaml file"
+        exit 1
+    fi
+else
+    echo "The file is not a yaml file"
+    exit 1
+fi
+
+# if the first option is -c, --content, display the content of the yaml file
+if [ $1 == "-c" ] || [ $1 == "--content" ]; then
+    yq $file
+fi
+
+# if the first option is -k, --keys, display the keys of the yaml file
+if [ $1 == "-k" ] || [ $1 == "--keys" ]; then
+    yq 'to_entries | .[] | .key' $file
+fi
+
+# if the first option is -v, --values, display the values of the yaml file
+if [ $1 == "-v" ] || [ $1 == "--values" ]; then
+    yq 'to_entries | .[] | .value' $file
+fi
+
+
+if [ $1 == "-kv" ] || [ $1 == "--keyvalue" ]; then
+    # check if the number of arguments is less than 3
+    if [ $# -lt 3 ]; then
+        echo "The key name is missing"
+        exit 1
+    fi
+    yq 'to_entries | .[] | select(.key == "'$3'") | .value' $file
+fi
+
+# if the first option is -vr, --valuereplace, replace the value of the key
+if [ $1 == "-vr" ] || [ $1 == "--valuereplace" ]; then
+    if [ $# -lt 4 ]; then
+        echo "The key name or the value is missing"
+        exit 1
+    fi
+    yq 'to_entries | .[] | select(.key == "'$3'") | .value = "'$4'"' $file
+fi
